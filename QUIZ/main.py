149f61cfd485cd17b0b9a5bde927a3dc11cbe705
Priscilla Alves <perealvees@gmail.com>
2022-10-25 21:05:04 -0300

    adicionando pontos

--- QUIZ/main.py
+++ QUIZ/main.py
@@ -3,6 +3,8 @@ answer_user = input('Bora começar um desafio? (Y/N) ')
 print(answer_user)
 if answer_user != "Y":
     quit()
+score = 0
+
 
 print('Começando ... ')
 print('Quem desenvolveu o jogo GTA? \nA) RockStar Games \nB) Activision \nC) EA Games')
@@ -10,6 +12,7 @@ answer_user_1 = input('Resposta: ')
 
 if answer_user_1 == 'A':
     print('Correto!')
+    score = score + 1
 else:
     print('Resposta Incorreta')
 
@@ -18,7 +21,9 @@ answer_user_2 = input('Resposta: ')
 
 if answer_user_2 == 'C':
     print('Correto!')
+    score = score + 1
 else:
     print('Resposta Incorreta')
 
+print(f"Fim de Jogo... Pontuação: { score } /2" )
 
